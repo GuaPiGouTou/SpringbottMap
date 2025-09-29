50f58b0ffdf782a525cc247672d85ac0bed744f8
guapi <1798196043@qq.com>
2025-09-29 14:47:06 +0800

    2025-9-29

--- src/main/java/com/tencent/wxcloudrun/model/Room.java
+++ src/main/java/com/tencent/wxcloudrun/model/Room.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.security.Timestamp;
+import java.util.List;
 import java.util.Map;
 
 @Data
@@ -17,8 +18,8 @@ public class Room {
     private BigDecimal RoomArea;//房间面积
     private String RoomType;//房型
     private Map<String, BigDecimal> RoomPayment;// 支付方式与价格映射
-    private boolean[] RoomFurniture;// 设施信息 - 建议使用boolean数组或List<Boolean>
-    private String[] RoomVideo;// 媒体上传后的UID组 - 建议使用数组或List
+    private List<Boolean> RoomFurniture;// 设施信息 - 建议使用boolean数组或List<Boolean>
+    private List<String> RoomVideo;// 媒体上传后的UID组 - 建议使用数组或List
     private Long PropertyId; //所属房源ID
 }
 
