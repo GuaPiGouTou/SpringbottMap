e98ea2458f36c9df4f7a6f9a38664629081e2d43
guapi <1798196043@qq.com>
2025-09-29 15:55:17 +0800

    2025-9-29-insert2

--- src/main/java/com/tencent/wxcloudrun/controller/RoomController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/RoomController.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,7 +26,7 @@ public class RoomController {
 
     }
     @PostMapping(value = "/api/room/insert")
-    public JsonResult<String> insertroom(Room  room)
+    public JsonResult<String> insertroom(@RequestBody Room  room)
     {
         int count = roomService.insertRoom(room);
         if(count>0)
